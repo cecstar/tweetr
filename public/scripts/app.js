45ff09b95afcaa1f8dd92a7609ec9820bb232ed5
cecstar <yocecia@gmail.com>
2016-10-20 22:59:51 +0000

    tweets rendering, icons not appending to new tweets - needs to be fixed, dynamic tweets working.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,8 +15,10 @@ $(document).ready(function() {
     var $tweetContent = $("<main>").addClass("tweetContent").html(tweetObject.content.text);
     var formattedDate = new Date(1*tweetObject.created_at);
     var $footerContent = $("<span>").addClass("footer").html(formattedDate.toUTCString());
-    var $footer = $("<footer>").append($footerContent);
 
+    // var $footerIcons = $("<footer>").addClass("<i>");**FIX THIS****
+
+    var $footer = $("<footer>").append($footerContent)
 
     $header.append($img);
     $header.append($name);
@@ -27,23 +29,62 @@ $(document).ready(function() {
 
     return $tweet;
   }
-  var arr = createTweetElement(testTweet);
-  $("#feed").append(arr);
-});
 
-var testTweet = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+// Fake data taken from tweets.json
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
+function renderTweets(array) {
+  for( var i in array ) {
+  var $newTweet = createTweetElement(array[i]);
+  $("#feed").append($newTweet);
+  }
+}
+
+renderTweets(data);
+
+});
