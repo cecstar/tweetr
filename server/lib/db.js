cb37d952549bd2d50e6342f27bcec07a9d91c848
cecstar <yocecia@gmail.com>
2016-10-25 17:24:56 +0000

    Fix mongo connection string issue

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI;
 
+// const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
 let collection;
 
   const Tweets = {
